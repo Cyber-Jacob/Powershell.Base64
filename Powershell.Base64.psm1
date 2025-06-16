90244478f01cdb3e4bb7bccee16c1b9479a2294c
cyber-jacob <cybervaquero@outlook.com>
2025-06-15 20:40:12 -0500

    Update Powershell.Base64.psm1 module examples and cmdlet naming to be explicit. Specify Output Type explicitly ConvertFrom cmdlet. Create README.md with explanation and usage isntructions.

--- Powershell.Base64.psm1
+++ Powershell.Base64.psm1
@@ -8,29 +8,21 @@ The user-supplied string to convert to Base64.
 .PARAMETER Encoding
 The explicit encoding of the supplied input. In many cases is not necesarry, but can be useful if working with different character encodings.
 
-.EXAMPLE 
+.EXAMPLE
 # Convert the string, "Hello, World!" to Base64 while explicitly specifying the supplied text encoding as ASCII
-ConvertTo-Base64 -String "Hello World!" -Encoding "ASCII"
+ConvertTo-Base64String -String "Hello World!" -Encoding "ASCII"
 
 .EXAMPLE
 # Convert the string "Привіт, світ!" to Base64 while explicitly specifying the supplied text encoding as UTF8. UTF8 is the default switch parameter, so this is not necesarry but is provided for sake of example.
-ConvertTo-Base64 -String "Привіт, світ!" -Encoding "UTF8"
-
-.EXAMPLE
-# Convert the Base64 string, "0J/RgNC40LLRltGCLCDRgdCy0ZbRgiE=" to UTF8 text while explicitly specifying UTF8 Encoding.
-ConvertFrom-Base64 -Base64String "0J/RgNC40LLRltGCLCDRgdCy0ZbRgiE=" -Encoding "UTF8"
-
-.EXAMPLE 
-# Convert the Base64 string, "PwRABDgEMgRWBEIEIABBBDIEVgRCBA==" to Unicode text while explicitly specifying Unicode encoding.
-ConvertFrom-Base64 -Base64String "PwRABDgEMgRWBEIEIABBBDIEVgRCBA==" -Encoding "Unicode"
+ConvertTo-Base64String -String "Привіт, світ!" -Encoding "UTF8"
 #>
-function ConvertTo-Base64 {
-
+function ConvertTo-Base64String {
+    [OutputType([string])]
     [CmdletBinding()]
     param (
         [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
         [string]$String,
-    
+
         [Parameter()]
         [ValidateSet('UTF8', 'ASCII', 'Unicode')]
         [string]$Encoding = 'UTF8'
@@ -40,17 +32,37 @@ function ConvertTo-Base64 {
         switch ($Encoding) {
             'UTF8'      {$encodebytes = [System.Text.Encoding]::UTF8.GetBytes($String)}
             'ASCII'     {$encodebytes = [System.Text.Encoding]::ASCII.GetBytes($String)}
-            'Unicode'   {$encodebytes = [System.Text.Encoding]::Unicode.GetBytes($String)}   
+            'Unicode'   {$encodebytes = [System.Text.Encoding]::Unicode.GetBytes($String)}
         }
-        
         [Convert]::ToBase64String($encodebytes)
-        
     }
 }
 
-function ConvertFrom-Base64 {
-    
+<#
+.SYNOPSIS
+Converts input from a Base64 String. This function is intended to support explicit typing of the input text via an encoding parameter. In most cases UTF8 should be used. However, support for ASCII and Unicode are included for one-way conversions to or from Base64 for sake of usability, if required.
+
+.PARAMETER String
+The user-supplied string to convert to Base64.
+
+.PARAMETER Encoding
+The explicit encoding of the supplied input. In many cases is not necesarry, but can be useful if working with different character encodings.
+
+.PARAMETER ErrorOnInvalidInput
+A switch parameter to throw an error and terminate on user-supplied strings that are not valid Base64 strings.
+
+.EXAMPLE
+# Convert the Base64 string, "0J/RgNC40LLRltGCLCDRgdCy0ZbRgiE=" to UTF8 text while explicitly specifying UTF8 Encoding.
+ConvertFrom-Base64String -Base64String "0J/RgNC40LLRltGCLCDRgdCy0ZbRgiE=" -Encoding "UTF8"
+
+.EXAMPLE
+# Convert the Base64 string, "PwRABDgEMgRWBEIEIABBBDIEVgRCBA==" to Unicode text while explicitly specifying Unicode encoding.
+ConvertFrom-Base64String -Base64String "PwRABDgEMgRWBEIEIABBBDIEVgRCBA==" -Encoding "Unicode"
+#>
+
+function ConvertFrom-Base64String {
     [CmdletBinding()]
+    [OutputType([string])]
     param(
         [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
         [string]$Base64String,
@@ -69,9 +81,9 @@ function ConvertFrom-Base64 {
                 'UTF8'      {[System.Text.Encoding]::UTF8.GetString($OutputBytes)}
                 'ASCII'     {[System.Text.Encoding]::ASCII.GetString($OutputBytes)}
                 'Unicode'   {[System.Text.Encoding]::Unicode.GetString($OutputBytes)}
-            }            
+            }
         }
-        catch { 
+        catch {
             if ($ErrorOnInvalidInput) {
                 throw "Valid input was not provided."
             }
@@ -79,8 +91,8 @@ function ConvertFrom-Base64 {
                 Write-Warning "Input is not valid Base64."
             }
         }
-    }    
+    }
 }
 
-Export-ModuleMember -Function ConvertTo-Base64
-Export-ModuleMember -Function ConvertFrom-Base64
+Export-ModuleMember -Function ConvertTo-Base64String
+Export-ModuleMember -Function ConvertFrom-Base64String
---
+++ README.md
@@ -0,0 +1,28 @@
+# Powershell.Base64 Module
+This Powershell module exists to convert to and from Base64 strings using UTF8, ASCII, and Unicode character encodings. For almost all cases, UTF8 - the default encoding option - should be used. However, the optional Encoding parameter is present for sake of functionality. 
+
+## Usage
+``` Powershell
+
+# Convert the string, "Hello, World!" to Base64 while explicitly specifying the supplied text encoding as ASCII
+ConvertTo-Base64String -String "Hello World!" -Encoding "ASCII"
+
+# Convert the string "Привіт, світ!" to Base64 while explicitly specifying the supplied text encoding as UTF8. UTF8 is the default switch parameter, so this is not necesarry but is provided for sake of example.
+ConvertTo-Base64String -String "Привіт, світ!" -Encoding "UTF8"
+
+# Convert the Base64 string, "0J/RgNC40LLRltGCLCDRgdCy0ZbRgiE=" to UTF8 text while explicitly specifying UTF8 Encoding.
+ConvertFrom-Base64String -Base64String "0J/RgNC40LLRltGCLCDRgdCy0ZbRgiE=" -Encoding "UTF8"
+
+# Convert the Base64 string, "PwRABDgEMgRWBEIEIABBBDIEVgRCBA==" to Unicode text while explicitly specifying Unicode encoding.
+ConvertFrom-Base64String -Base64String "PwRABDgEMgRWBEIEIABBBDIEVgRCBA==" -Encoding "Unicode"
+
+# Convert a user-supplied string to Base64 using Read-Host and sending as pipeline-input to a Convert statement.
+$string = Read-Host("Enter string to be converted to Base64: ") | ConvertTo-Base64String
+
+# Convert a variable, $string, to Base64 specifying the string encoding as Unicode.
+ConvertTo-Base64String -String $string -Encoding "Unicode"
+
+# Convert a Base64 string variable, $string, to cleartext specifying the destination encoding as Unicode.
+ConvertFrom-Base64String -String $string -Encoding "Unicode"
+
+```
